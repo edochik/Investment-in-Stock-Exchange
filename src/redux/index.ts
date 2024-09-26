d19599917831f065386532ed8ba2d459a82feaad
Egor Toropov <edoch@yandex.ru>
2024-09-26 11:24:53 +0300

    update coefficientSlice

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { dataSlice } from './dataSlice/dataSlice'
 import { fetchInitialDataThunk } from './dataSlice/thunk';
 import { sumStocksSlice } from './sumStocksSlice/sumStocksSlice';
-import { coefficientSlice } from './coefficientSlice/coefficientSlice';
+import { weightCompaniesSlice } from './coefficientSlice/coefficientSlice';
 import { tableSlice } from './tableSlice/tableSlice';
 
 export const store = configureStore({
@@ -10,7 +10,7 @@ export const store = configureStore({
 		data: dataSlice.reducer,
 		table: tableSlice.reducer,
 		sumStocks: sumStocksSlice.reducer,
-		coefficient: coefficientSlice.reducer,
+		weights: weightCompaniesSlice.reducer,
 	},
 })
 
