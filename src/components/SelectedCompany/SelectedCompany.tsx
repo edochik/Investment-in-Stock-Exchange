7b401607bb23b28a2be8ede05a9334e0b37622f3
Egor Toropov <edoch@yandex.ru>
2024-10-11 12:08:51 +0300

    update autcomplete logic

--- src/components/SelectedCompany/SelectedCompany.tsx
+++ src/components/SelectedCompany/SelectedCompany.tsx
@@ -1,29 +1,25 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import s from "./SelectedCompany.module.scss";
 import { useAppDispatch, useAppSelector } from "../../hooks";
-import { getFilterCompany } from "./getFilterCompany";
+import { filterByImoex } from "./filterByImoex";
 import { selectedNonImoex } from "../../redux/nonImoexCompanySlice/nonImoexCompanySlice";
-import classNames from "classnames";
+import { Autocomplete } from "../Autocomplete/Autocomplete";
+import { filterByKey } from "./filterByKey";
+
 const SelectedCompany = () => {
-  const [inputValues, setInputValues] = useState("");
   const [ticker, setTicker] = useState("");
-  console.log(ticker);
-  const [userWeight, setUserWeight] = useState("");
+  const [inputWeight, setInputWeight] = useState("");
   const dispatch = useAppDispatch();
   const { securities, imoex } = useAppSelector((state) => state.data);
   const nonImoexCompany = useAppSelector((state) => state.nonImoexCompany);
-  const companies = getFilterCompany(securities, imoex.concat(nonImoexCompany));
+  const companies = filterByImoex(securities, imoex.concat(nonImoexCompany));
 
-  const onClickSelectedCompany = (
-    e: React.MouseEvent<HTMLLIElement, MouseEvent>,
-    ticker: string
+  const [cleanInput, setCleanInput] = useState(false);
+  const refBtnAddCompany = useRef(null);
+  console.log(ticker);
+  const onClickAddCompany = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
-    const target = e.target as HTMLElement;
-    setTicker(securities[ticker].secid);
-    setInputValues(target.innerText);
-  };
-
-  const onClickAddCompany = () => {
     const { shortname, secid, prevdate } = securities[ticker];
     dispatch(
       selectedNonImoex({
@@ -32,13 +28,15 @@ const SelectedCompany = () => {
         ticker: secid,
         shortnames: shortname,
         secids: secid,
-        weight: Number(userWeight),
+        weight: Number(inputWeight),
         tradingsession: 0,
       })
     );
-    setInputValues("");
+    if (refBtnAddCompany.current && refBtnAddCompany.current === event.target) {
+      setCleanInput(true);
+    }
     setTicker("");
-    setUserWeight("");
+    setInputWeight("");
   };
 
   const handleChangeWeight = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -48,47 +46,23 @@ const SelectedCompany = () => {
     }
     const valueAsNumber = Number(value);
     if (!Number.isNaN(valueAsNumber)) {
-      setUserWeight(value);
+      setInputWeight(value);
     }
   };
+
   return (
     <div className={s.SelectedCompany}>
       <div className={s.wrapper}>
         <label className={s.label}>
           Введите название:
           <div className={s.inner}>
-            <input
-              className={s.input}
-              type="text"
-              value={inputValues}
-              onChange={(e) => setInputValues(e.target.value)}
+            <Autocomplete
+              list={companies}
+              filterByKey={filterByKey}
+              setTicker={setTicker}
+              cleanInput={cleanInput}
+              setCleanInput={setCleanInput}
             />
-            <ul className={s.list}>
-              {inputValues.length > 0 &&
-                companies
-                  .filter(
-                    (company) =>
-                      company.shortname
-                        .toLocaleLowerCase()
-                        .includes(inputValues.toLocaleLowerCase()) ||
-                      company.secid
-                        .toLocaleLowerCase()
-                        .includes(inputValues.toLocaleLowerCase())
-                  )
-                  .map((company, index) =>
-                    company.shortname !== inputValues ? (
-                      <li
-                        key={company.shortname}
-                        className={s.company}
-                        onClick={(e) =>
-                          onClickSelectedCompany(e, company.secid)
-                        }
-                      >
-                        {company.secid} {company.shortname}
-                      </li>
-                    ) : null
-                  )}
-            </ul>
           </div>
         </label>
         <label className={s.label}>
@@ -96,14 +70,15 @@ const SelectedCompany = () => {
           <input
             className={s.input}
             type="text"
-            value={userWeight}
+            value={inputWeight}
             onChange={(e) => handleChangeWeight(e)}
           />
         </label>
         <button
+          ref={refBtnAddCompany}
           className={s.btn_add}
           onClick={onClickAddCompany}
-          disabled={!securities[ticker] || userWeight.length === 0}
+          disabled={!securities[ticker] || inputWeight.length === 0}
         >
           Добавить компанию
         </button>
@@ -113,3 +88,15 @@ const SelectedCompany = () => {
 };
 
 export { SelectedCompany };
+
+// parent
+// button in parent => press
+// if press button => true =>
+
+// children
+// input in children => clear
+// useEffect(() =>{
+// if(true){
+// clean input
+// }
+//} ,[])
---
+++ src/components/SelectedCompany/filterByKey.ts
@@ -0,0 +1,10 @@
+import { Security } from "../../domain/Security.js";
+
+export const filterByKey = (security: Security, value: string) => {
+
+	return (
+		// security.shortname.toLowerCase().startsWith(value.toLowerCase()) || ??
+		// security.secid.toLowerCase().startsWith(value.toLowerCase()) || ??
+		(security.secid.toLowerCase() + " " + security.shortname.toLowerCase()).includes(value.toLowerCase())
+	);
+};
