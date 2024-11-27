7589e5bf4802dcf9dfe8a62609d3d4ff7d9d7e37
Egor Toropov <edoch@yandex.ru>
2024-11-27 20:35:45 +0300

    update component UserMoneyInput

--- src/components/UserMoneyInput/UserMoneyInput.tsx
+++ src/components/UserMoneyInput/UserMoneyInput.tsx
@@ -2,6 +2,7 @@ import s from "./UserMoneyInput.module.scss";
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { updateUserMoney } from "../../redux/userDataSlice/userDataSlice";
 import { formatPrice } from "./formatPrice";
+import classNames from "classnames";
 
 const UserMoneyInput = () => {
   const money = useAppSelector((state) => state.userData.moneyUser);
@@ -21,8 +22,23 @@ const UserMoneyInput = () => {
         value={formatPrice(money)}
         onChange={(e) => handleChangeUpdateMoney(e)}
       />
+      <span
+        className={classNames({
+          [s.info]: true,
+          [s.hidden]: money > 9999,
+        })}
+        aria-label={money < 9999 ? "Введите сумму от 10 000 рублей" : ""}
+      >
+        *Введите сумму от 10 000 рублей
+      </span>
     </label>
   );
 };
+
+// className={classNames({
+//   [s.button]: true,
+//   [s.show]: column === selectedColumn.column,
+//   [s.rotate]: selectedColumn.direction === "desc",
+// })}
 
 export { UserMoneyInput };
